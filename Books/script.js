26ea2a53ea9f8e13c486ce3a385d59e6e92c7ab5
Emin Devrim Fidan <devrimfidan@users.noreply.github.com>
2025-04-10 21:40:47 +0200

    Update script.js

--- Books/script.js
+++ Books/script.js
@@ -1,11 +1,11 @@
-const proxyUrl = "https://api.allorigins.win/get?url=";
+const proxyUrl = "https://thingproxy.freeboard.io/fetch/";
 const feedUrl = "https://www.goodreads.com/review/list_rss/82726216?shelf=read";
 
 fetch(proxyUrl + encodeURIComponent(feedUrl))
-  .then(response => response.json())
-  .then(data => {
-    console.log("Fetched data:", data.contents);
-    document.getElementById("book-list").innerText = data.contents ? "Data loaded!" : "No content found.";
+  .then(response => response.text())
+  .then(xmlString => {
+    console.log("Fetched data:", xmlString);
+    document.getElementById("book-list").innerText = "Data loaded!";
   })
   .catch(err => {
     console.error("Fetch failed:", err);
